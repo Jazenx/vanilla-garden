685a0af2b0db0442e1dddfa8f5ae1043f3378c89
JasonZhour <jason.zhour@gmail.com>
2019-08-02 17:48:31 +0800

    feat(init): 初始化

---
+++ src/app.jsx
@@ -0,0 +1,40 @@
+import '@tarojs/async-await'
+import Taro, { Component } from '@tarojs/taro'
+import { Provider } from '@tarojs/redux'
+
+import Index from './pages/index'
+
+import configStore from './store'
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+const store = configStore()
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    }
+  }
+
+  render () {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
